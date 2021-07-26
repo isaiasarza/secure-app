c0b11396417bd14a4986444765593c338843da40
Daniel Isaías Arza <DArza@emergencias.com.ar>
2021-07-26 20:17:20 -0300

    adding login test

--- src/components/login/Login.test.tsx
+++ src/components/login/Login.test.tsx
@@ -1,54 +1,37 @@
-import { render, screen, findByText, fireEvent } from '@testing-library/react';
-import { ionFireEvent  } from "@ionic/react-test-utils";
+import { render, screen, fireEvent } from "@testing-library/react";
+
 import LoginComponent from "./Login";
 
 import { act } from "react-dom/test-utils";
 
-
 describe("LoginComponent", () => {
- 
   beforeEach(async () => {
-    const { findByTitle, findByText } = render(<LoginComponent ></LoginComponent>)
-    //await act(async () =>{render(<LoginComponent ></LoginComponent>)});
-    
-    
+    render(<LoginComponent></LoginComponent>);
   });
 
-  
-
   test("the submit_button should be dfasfasdf", async () => {
-    const email = screen.getByTestId("email") ;
+    const email = screen.getByTestId("email");
     const password = screen.getByTestId("password");
-    const button = screen.getByTestId("submit_button");
-    act(()=>{
-      fireEvent.change(email,{
-        target:{
-          detail:{
-            value:"arza.isaias@gmail.com"
-          }
-        }
-      })
-  
-     //console.log(button.ge)
-      
-    })
-    expect((email as any).detail.value).toBe("arza.isaias@gmail.com")
-    act(()=>{
-      fireEvent.change(password,{
-        target:{
-          detail:{
-            value:"hahaha"
-          }
-        }
-      })
-    })
-   
-   expect((password as any).detail.value).toBe("hahaha")
+    act(() => {
+      fireEvent.change(email, {
+        target: {
+          detail: {
+            value: "arza.isaias@gmail.com",
+          },
+        },
+      });
+    });
+    expect((email as any).detail.value).toBe("arza.isaias@gmail.com");
+    act(() => {
+      fireEvent.change(password, {
+        target: {
+          detail: {
+            value: "hahaha",
+          },
+        },
+      });
+    });
 
-    
-  })
-
-  
-
- 
+    expect((password as any).detail.value).toBe("hahaha");
+  });
 });
